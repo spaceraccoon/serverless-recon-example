335a7e646675ffc6be5131a8224178e9c7b05531
Eugene Lim <limzhiweieugene@gmail.com>
2020-05-15 21:18:22 +0800

    Add calling wrapper

--- crt-sh/app.py
+++ crt-sh/app.py
@@ -27,20 +27,13 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    try:
-        ip = requests.get("http://checkip.amazonaws.com/")
-    except requests.RequestException as e:
-        # Send some context about this error to Lambda Logs
-        print(e)
-
-        raise e
-
-    if "queryStringParameters" not in event or "domain" not in event["queryStringParameters"]:
+    if "domain" not in event:
         return {
             "statusCode": 400,
-            "body": json.dumps({
-                "error": "Requires domain parameter."
-            }),
+            "body": {
+                "error": "Requires domain parameter.",
+                "subdomains": []
+            },
         }
 
     try:
@@ -49,7 +42,7 @@ def lambda_handler(event, context):
 
         cursor = connection.cursor()
 
-        cursor.execute(sql.SQL("SELECT DISTINCT NAME_VALUE FROM certificate_identity ci WHERE ci.NAME_TYPE = 'dNSName' AND reverse(lower(ci.NAME_VALUE)) LIKE reverse(lower('%.{}'));".format(event["queryStringParameters"]["domain"])))
+        cursor.execute(sql.SQL("SELECT DISTINCT NAME_VALUE FROM certificate_identity ci WHERE ci.NAME_TYPE = 'dNSName' AND reverse(lower(ci.NAME_VALUE)) LIKE reverse(lower('%.{}'));".format(event["domain"])))
         records = [r[0] for r in cursor.fetchall()]
     except (Exception, psycopg2.Error) as e :
         print ("Error while connecting to PostgreSQL", e)
@@ -61,8 +54,8 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "domain": event["queryStringParameters"],
-            "subdomains": records,
-        }),
+        "body": {
+            "error": "",
+            "subdomains": records
+        },
     }
